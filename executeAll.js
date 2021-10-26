94eabe03ff95c1aa9a9a0bbc79986b86864f5319
“JaredMathis” <wordslikejared@gmail.com>
2021-10-26 07:43:56 -0400

    touch t

--- executeAll.js
+++ executeAll.js
@@ -2,6 +2,8 @@ const executeThen = require("./executeThen");
 
 module.exports = executeAll;
 function executeAll(commands, then) {
+    console.log('executeAll');
+    console.log({commands})
     executeThen(commands[0], () => {
         commands.splice(0, 1);
         if (commands.length) {
