60562f18c223e7cbcd1ec2df53a000d707b4fdaa
“JaredMathis” <wordslikejared@gmail.com>
2021-10-26 07:46:19 -0400

    touch t

--- executeAll.js
+++ executeAll.js
@@ -5,7 +5,6 @@ function executeAll(commands, then) {
     console.log('executeAll');
     console.log({commands})
     executeThen(commands[0], () => {
-        console.log('here')
         commands.splice(0, 1);
         if (commands.length) {
             executeAll(commands);
